44991a67519f17d48d3f446b1383f2531bfa7977
hamid <hamid>
2016-12-12 16:58:27 +0330

    xana 4

--- Untitled2.c
+++ Untitled2.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    printf("Helloff woccrld");
-    return 0;
+    int i, j, n, b;
+    scanf("%d",&n);
+    for(i=2 ; i < n ; ++i){
+    	if(n / i == 0)
+    	b++;
+	}
+	if(b == 0)
+		printf("is prime");
+	else{
+		printf("no prime");
+	}	
 }
